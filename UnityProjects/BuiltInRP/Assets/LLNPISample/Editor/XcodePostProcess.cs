0c414ad312668f96074583f06865e65ac7c2910b
mao <miymao78.d@gmail.com>
2022-12-05 02:29:01 +0900

    refactor: ソース整理

--- UnityProjects/BuiltInRP/Assets/LLNPISample/Editor/XcodePostProcess.cs
+++ UnityProjects/BuiltInRP/Assets/LLNPISample/Editor/XcodePostProcess.cs
@@ -9,16 +9,16 @@ namespace LLNPISample.Editor
     internal static class XcodePostProcess
     {
         [PostProcessBuild]
-        private static void OnPostProcessBuild(BuildTarget target, string path)
+        private static void OnPostProcessBuild(BuildTarget target, string xcodeProjPath)
         {
             if (target != BuildTarget.iOS) return;
 
-            var projectPath = PBXProject.GetPBXProjectPath(path);
+            var projectPath = PBXProject.GetPBXProjectPath(xcodeProjPath);
             var project = new PBXProject();
             project.ReadFromString(File.ReadAllText(projectPath));
 
             // 検証用に常に有効にしておく
-            var schemePath = $"{path}/Unity-iPhone.xcodeproj/xcshareddata/xcschemes/Unity-iPhone.xcscheme";
+            var schemePath = $"{xcodeProjPath}/Unity-iPhone.xcodeproj/xcshareddata/xcschemes/Unity-iPhone.xcscheme";
             var xcScheme = new XcScheme();
             xcScheme.ReadFromFile(schemePath);
             xcScheme.SetFrameCaptureModeOnRun(XcScheme.FrameCaptureMode.Metal);
--- UnityProjects/BuiltInRP/Assets/LLNPISample/Plugins/LLNPIWithMetal/Editor/XcodePostProcess.cs
+++ UnityProjects/BuiltInRP/Assets/LLNPISample/Plugins/LLNPIWithMetal/Editor/XcodePostProcess.cs
@@ -10,29 +10,34 @@ namespace LLNPISample.Plugins.LLNPIWithMetal.Editor
     internal static class XcodePostProcess
     {
         [PostProcessBuild]
-        private static void OnPostProcessBuild(BuildTarget target, string path)
+        private static void OnPostProcessBuild(BuildTarget target, string xcodeProjPath)
         {
             if (target != BuildTarget.iOS) return;
 
-            var projectPath = PBXProject.GetPBXProjectPath(path);
+            var projectPath = PBXProject.GetPBXProjectPath(xcodeProjPath);
             var project = new PBXProject();
             project.ReadFromString(File.ReadAllText(projectPath));
 
+            AddMetalShader(xcodeProjPath, ref project);
+
+            File.WriteAllText(projectPath, project.WriteToString());
+        }
+
+        private static void AddMetalShader(string xcodeProjPath, ref PBXProject project)
+        {
             // `.metal` はAssets以下にあっても自動でxcodeprojに追加されないっぽいので、手動でコピーしてプロジェクトに足してやる。
             const string shaderPath = "/LLNPISample/Plugins/LLNPIWithMetal/Native/Shader.metal";
             const string nativePath = "/Libraries" + shaderPath;
 
             // 1. 先ずはビルド結果にあるプラグインが配置される場所と同じところにコピー
             var srcPath = Application.dataPath + shaderPath;
-            var dstPath = path + nativePath;
+            var dstPath = xcodeProjPath + nativePath;
             File.Copy(srcPath, dstPath, true);
 
             // 2. xcodeprojにファイルとして追加し、ターゲットに含めてやる
             var frameworkGuid = project.GetUnityFrameworkTargetGuid();
             var file = project.AddFile(dstPath, nativePath, PBXSourceTree.Source);
             project.AddFileToBuild(frameworkGuid, file);
-
-            File.WriteAllText(projectPath, project.WriteToString());
         }
     }
 }
