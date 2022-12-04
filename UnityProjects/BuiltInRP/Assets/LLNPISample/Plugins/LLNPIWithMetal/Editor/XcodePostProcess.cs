6b623fbf4bf5cccbe4a88f6680ab7c8f840939fa
mao <miymao78.d@gmail.com>
2022-12-04 23:14:09 +0900

    feat: .metalをxcodeprojに含めるようにした

---
+++ UnityProjects/BuiltInRP/Assets/LLNPISample/Plugins/LLNPIWithMetal/Editor/XcodePostProcess.cs
@@ -0,0 +1,40 @@
+#if UNITY_IOS
+using System.IO;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEditor.iOS.Xcode;
+using UnityEngine;
+
+namespace LLNPISample.Plugins.LLNPIWithMetal.Editor
+{
+    internal static class XcodePostProcess
+    {
+        [PostProcessBuild]
+        private static void OnPostProcessBuild(BuildTarget target, string path)
+        {
+            if (target != BuildTarget.iOS) return;
+
+            var projectPath = PBXProject.GetPBXProjectPath(path);
+            var project = new PBXProject();
+            project.ReadFromString(File.ReadAllText(projectPath));
+
+            // `.metal` はAssets以下にあっても自動でxcodeprojに追加されないっぽいので、手動でコピーしてプロジェクトに足してやる。
+            const string shaderPath = "/LLNPISample/Plugins/LLNPIWithMetal/Native/Shader.metal";
+            const string nativePath = "/Libraries" + shaderPath;
+
+            // 1. 先ずはビルド結果にあるプラグインが配置される場所と同じところにコピー
+            var srcPath = Application.dataPath + shaderPath;
+            var dstPath = path + nativePath;
+            File.Copy(srcPath, dstPath);
+
+            // 2. xcodeprojにファイルとして追加し、ターゲットに含めてやる
+            var frameworkGuid = project.GetUnityFrameworkTargetGuid();
+            var file = project.AddFile(dstPath, nativePath, PBXSourceTree.Source);
+            project.AddFileToBuild(frameworkGuid, file);
+
+            File.WriteAllText(projectPath, project.WriteToString());
+        }
+    }
+}
+
+#endif
