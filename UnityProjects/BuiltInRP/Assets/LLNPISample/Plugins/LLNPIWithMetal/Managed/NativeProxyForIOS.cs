66605f466bdbb2b2452ecf9a6d8550d0d9a2c627
mao <miymao78.d@gmail.com>
2022-12-04 21:57:57 +0900

    feat: 呼び出し周りの実装

---
+++ UnityProjects/BuiltInRP/Assets/LLNPISample/Plugins/LLNPIWithMetal/Managed/NativeProxyForIOS.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+namespace LLNPISample.Plugins.LLNPIWithMetal.Managed
+{
+    public sealed class NativeProxyForIOS : INativeProxy
+    {
+        public void DoExtraDrawCall()
+        {
+            CallRenderEventFunc(EventType.ExtraDrawCall);
+        }
+
+        public void DoCopyRT(RenderTexture srcRT, RenderTexture dstRT)
+        {
+            [DllImport("__Internal", EntryPoint = "setRTCopyTargets")]
+            static extern void SetRTCopyTargets(IntPtr src, IntPtr dst);
+
+            var src = srcRT ? srcRT.colorBuffer : Display.main.colorBuffer;
+            var dst = dstRT ? dstRT.colorBuffer : Display.main.colorBuffer;
+            SetRTCopyTargets(src.GetNativeRenderBufferPtr(), dst.GetNativeRenderBufferPtr());
+
+            CallRenderEventFunc(EventType.CopyRTtoRT);
+        }
+
+        // we will do several pretty useless events to show the usage of all api functions
+        private enum EventType
+        {
+            // will do an extra draw call to currently setup rt with custom shader
+            ExtraDrawCall = 0,
+
+            // copy src rt to internal texture and draws rect using it to dst r
+            CopyRTtoRT,
+        }
+
+        private static void CallRenderEventFunc(EventType eventType)
+        {
+            [DllImport("__Internal", EntryPoint = "getRenderEventFunc")]
+            static extern IntPtr GetRenderEventFunc();
+
+            GL.IssuePluginEvent(GetRenderEventFunc(), (int)eventType);
+        }
+    }
+}
